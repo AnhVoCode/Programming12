d04dcd10b81ea3dd32d658331c28846aa2264596
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-09-05 14:48:29 -0700

    test

--- Review/src/Maths.java
+++ Review/src/Maths.java
@@ -2,13 +2,26 @@ import java.util.Scanner;
 
 public class Maths {
     //Fields
-    int a = 0;
-    int b =0;
-    int c = 0;
-    int y = 0;
-    int x = 0;
-
-    //Methods
+    double a = 0;
+    double b =0;
+    double y = 0;
+    double x = 0;
     
+    Scanner in = new Scanner(System.in);
+
+    public void linearSolved(double a, double b){
+        a = in.nextDouble();
+        b = in.nextDouble();
+
+        if(a==0 && b ==0){
+            y =0;
+            System.out.println("x ");
+
+        }
+        else if(a == 0){
+            y = this.b;
+
+        }
+    }
 
 }
