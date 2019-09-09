89439b07bf60cc471afbd31fedd074aee7ce5d72
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-09-09 14:35:32 -0700

    test

--- Review/src/Main.java
+++ Review/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        //Trial for linear equation
         Problems a  = new Problems();
         a.linearEquation(2,4,6);
 
+        //Trial for quadratic equation
+        Problems b = new Problems();
+        b.quadraticEquation(-3,11,5);
     }
 }
--- Review/src/Problems.java
+++ Review/src/Problems.java
@@ -1,23 +1,21 @@
 public class Problems {
-    //Fields
-    double x = 0;
 
 
     //Methods
 
     //Solve linear equation in the form ax + b = c
     public void linearEquation(double a, double b, double c){
-
+        double ans;
         //if a = b = 0; x can be any real value
         if(a == 0 && b==0 && c==0) {
             System.out.println("Infinite solution");
         }
 
         else {
-            x = (c-b)/a;
+            ans = (c-b)/a;
 
             //if a = 0, x is undefined
-            System.out.println(x);
+            System.out.println(ans);
             if(a==0){
                 System.out.println("No solution");
             }
@@ -28,6 +26,33 @@ public class Problems {
 
     //Solve quadratic equation in the form ax^2 + bx + c = constant
     public void quadraticEquation(double a, double b, double c){
+        double root1 = 0;
+        double root2 = 0;
+        //if a = 0; x is undefined
+        if(a == 0){
+            System.out.println("No solution");
+        }
+        //find discriminant
+        double d = Math.pow(b,2) - 4*a*c;
 
+        //if discriminant < 0, no solution
+        if(d<0){
+            System.out.println("No real root");
+        }
+
+        //if discriminant > 0, there will be two different real roots
+       else if(d>0){
+           root1 = -b + Math.sqrt(d);
+           root2 = -b - Math.sqrt(d);
+            System.out.format("Root 1 = %.2f, r and Root 2 = %.2f", root1, root2);
+        }
+
+        //if discriminant = 0, there will be one or two same real roots.
+       else if(d == 0){
+           root1 = root2 = -b/(2*a);
+           System.out.format("Root 1 = Root 2 = %.2f",root1);
+        }
     }
+
+    
 }
