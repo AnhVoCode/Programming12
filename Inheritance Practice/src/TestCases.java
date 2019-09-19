264e46b2f52074bbc42b29d4666dabc2be8a8eff
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-09-19 15:03:00 -0700

    test

--- Inheritance Practice/src/TestCases.java
+++ Inheritance Practice/src/TestCases.java
@@ -3,9 +3,14 @@ import org.junit.Test;
 public class TestCases {
     Planet testPlanet;
     GasPlanet testGasPlanet;
+    Moon moon;
     RocketPlanet testRocketplanet:
 
-
+    public void setUp(){
+        testPlanet = new Planet(28, "Moon");
+        testGasPlanet = new GasPlanet();
+        testRocketplanet = new RocketPlanet();
+    }
     @Test
     public void testEqualsInPlanet(){
 
