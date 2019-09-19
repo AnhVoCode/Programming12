3eaeeeb31975d9389d7bd9cb267373d529f7d250
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-09-19 14:59:43 -0700

    test

---
+++ Inheritance Practice/src/TestCases.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class TestCases {
+    Planet testPlanet;
+    GasPlanet testGasPlanet;
+    RocketPlanet testRocketplanet:
+
+
+    @Test
+    public void testEqualsInPlanet(){
+
+
+    }
+
+}
