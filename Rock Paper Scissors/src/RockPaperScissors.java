49621301305019cf384edceea9b3c9a2fffee6a3
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-11-12 13:39:35 -0800

    test

--- Rock Paper Scissors/src/RockPaperScissors.java
+++ Rock Paper Scissors/src/RockPaperScissors.java
@@ -1,4 +1,10 @@
+import java.util.Random;
+
 public class RockPaperScissors {
+    private int computerChoice(){
+        Random number = new Random();
+        int computerChoice = number.nextInt(3)+1;
+    }
    public int startGames(int playerChoice){
        if(playerChoice<1 || playerChoice>3){
            
