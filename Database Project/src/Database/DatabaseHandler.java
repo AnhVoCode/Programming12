e99e7b2b52193c5e6497037c625ed150a48f39db
AnhVoProgramming12 <43790597+AnhVoProgramming12@users.noreply.github.com>
2020-03-09 11:35:10 -0700

    test

---
+++ Database Project/src/Database/DatabaseHandler.java
@@ -0,0 +1,25 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DatabaseHandler {
+    private static final String DB_url = "jdbc:derby:database/forum;create=true";
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    public DatabaseHandler(){
+        createConnection();
+    }
+
+    private void createConnection() {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+            conn = DriverManager.getConnection(DB_url);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ Database Project/src/UI/Controllers/AddStudent.java
@@ -0,0 +1,16 @@
+package UI.Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddStudent extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
